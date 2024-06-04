0a3a9a14b4e9396d21d33398028e93af74cb190b
BunyodbekOlimov <oybekivichbunyod@gmail.com>
2024-06-05 01:33:19 +0500

    start

---
+++ main.js
@@ -0,0 +1,60 @@
+// {
+//     1 chisi
+// let a = +prompt("a ga son kiriting")
+// alert(a * 4)
+// }
+
+// {
+//     2 chisi 
+//     let b = prompt("b ga son kiriting")
+//     alert(b * b)
+// }
+
+// {
+//     3 chisi
+//     let a = +prompt("eniga son kiriting") 
+//     let b = +prompt(" boyiga son kiriting") 
+//     alert("Yuzasi   " +( a * b ) +  "       Perimetri   " + (2 *(a + b)))
+// }
+
+// {
+//     4 chisi
+//     let d = prompt("diametriga son kiriting")
+//     p = 3.14
+//     alert(" Uzunligi  " + p * d)
+// }
+
+// {
+//     5 chisi
+//     let a = +prompt(" Koobning yon tomonini kiriting ")
+//     let v =  a * a * a
+//     let s =  6 *( a *a)
+//     alert("Hajmi  " + v  + " Tola sirti  "   +   s)
+// }
+
+// {
+//     6 chisi
+//     let a = +prompt("Paralelepepedning a son kiriting")
+//     let b = +prompt("Paralelepepedning b son kiriting")
+//     let c = +prompt("Paralelepepedning c son kiriting")
+//     let v = a * b * c
+//     let s = 2 * (a * b + b * c + a * c)
+//     alert(" Uning hajmi  " + v  + "  Sirti  "  +  s)
+// }
+
+// {
+//     7 chisi
+//     let r = prompt( "Doiraning radiusini kiriting ")
+//     let p = 3.14
+//     let l = 2 * p * r
+//     let s = p * (r * r)
+//     alert("  Uzunligi " + l  +  " Yuzasi  " + s)
+// }
+
+// {
+//     8 chisi
+//     let a = +prompt(" a son kiriting ")
+//     let b = +prompt(" b son kiriting ")
+//     let c = (a + b )/2
+//     alert( " O`rta arifmetigi " + c )
+// }
